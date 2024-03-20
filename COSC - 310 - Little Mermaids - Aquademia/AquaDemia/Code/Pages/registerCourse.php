f55c6bd5a18d10a6727a6db6b9ac8381b9630512
KendyID <157467253+KendyID@users.noreply.github.com>
2024-03-20 05:59:06 -0700

    Update registerCourse.php

---
+++ COSC - 310 - Little Mermaids - Aquademia/AquaDemia/Code/Pages/registerCourse.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register for a Course</title>
+    <link rel="stylesheet" href="../Assets/CSS/central.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <img src="../Assets/Images/logo.png" width="60" height="75" class="logo">
+            <navBar>
+                <navBarElements>
+                    <li><a href="#">Classes v</a></li>
+                    <li><a href="#">Assignments</a></li>
+                    <li><a href="#">Grades</a></li>
+                    <li><a href="#">Settings</a></li>
+                </navBarElements>
+            </navBar>
+        </div>
+    </header>
+    <div style="padding-left: 10%;">
+        <h1 style="padding-top: 5%;">Register for a Course</h1>
+        <form action="../Assets/PHP/registerCourseManager.php" method="post">
+            <input type="text" placeholder="Enter course ID" id="courseId" name="courseId"><br>
+            <button class="button button1" style="margin-top: 5%;">Register</button>
+        </form>
+    </div>
+</body>
+</html>
