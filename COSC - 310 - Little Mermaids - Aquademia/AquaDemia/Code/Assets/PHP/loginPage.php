f465f739444713c6bdebb53cdde0da8236256d46
BennettWitt <wittbennett@gmail.com>
2024-03-25 19:48:14 -0700

    Added functional phpunit test cases for loginPage.php

--- COSC - 310 - Little Mermaids - Aquademia/AquaDemia/Code/Assets/PHP/loginPage.php
+++ COSC - 310 - Little Mermaids - Aquademia/AquaDemia/Code/Assets/PHP/loginPage.php
@@ -17,30 +17,28 @@
                 $_SESSION["UserID"] = $user['UserID'];
                 $_SESSION["Username"] = $uname;
                 // Redirect to welcome page
-                echo "Welcome back " . $uname . " we are Happy to see you Again!";
         
                 if($user['UserType'] == "Admin"){
                     header("Location: ../../Pages/adminView.php");
-                    return "login error code:1";  //returns 1 if admin
+                    return "login code:1";  //returns 1 if admin
                 }
                 else if($user['UserType'] == "Student"){
                     header("Location: ../../Pages/studentView.php");
-                    return "login error code:2";  //returns 2 if student
+                    return "login code:2";  //returns 2 if student
                 }
-                else if($user['UserType'] == "Teacher"){
+                else if($user['UserType'] == "Professor"){
                     header("Location: ../../Pages/teacherView.php");
-                    return "login error code:3";  //returns 3 if teacher
+                    return "login code:3";  //returns 3 if teacher
                 }
                  } else {
                 // Display an error message
                 $alertMessage = "The password you entered was incorrect!";
-                echo $alertMessage;
                 header("Location: ../../Pages/login.html");
-                return "login error code:4"; //returns 4 if incorrect login info
+                return "login code:4->error"; //returns 4 if incorrect login info
                   }
         }
     }
-
+    session_save_path();
     session_start(); // Start the session.
     $servername = "localhost";
     $username = "root"; // default XAMPP MySQL username
