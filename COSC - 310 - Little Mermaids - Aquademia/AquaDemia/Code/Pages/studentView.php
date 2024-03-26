8b293dfa1507b87c37f8804c1fabcf22902b0a40
BennettWitt <wittbennett@gmail.com>
2024-03-26 10:23:41 -0700

    gave admin title in student veiw

--- COSC - 310 - Little Mermaids - Aquademia/AquaDemia/Code/Pages/studentView.php
+++ COSC - 310 - Little Mermaids - Aquademia/AquaDemia/Code/Pages/studentView.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin View</title>
+    <title>Student View</title>
     <link rel="stylesheet" href="../Assets/CSS/central.css">
 </head>
 <body>
